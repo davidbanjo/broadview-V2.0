8c42b65bdde11e84356ffbdc358f96f49e5eabb5
davidbanjo <davidbanjo879@gmail.com>
2024-05-20 20:18:40 +0100

    acertainity Ui configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Header from "./components/header/Header.jsx"
 import Footer from "./components/footer/Footer.jsx"
+import Navbar from './components/NavbarDemo.tsx'
 
 const App = () => {
+
+  
 
   return (
     <>
       <Header />
+      <Navbar />
       <Footer />
     </>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <div>
       <h1>Header</h1>
+      <h1 className="text-2xl font-bold underline">Hello world!</h1>;
     </div>
   )}
 
